1e28d78093cb0b15bcab1059c3ae1dbcaa29de83
vic huang <vic.huang1130x10@gmail.com>
2020-08-22 22:21:28 +0800

    test vps

--- app/index.js
+++ app/index.js
@@ -12,7 +12,7 @@ app.use(json());
 
 app.get("/SPIData", async (req, res) => {
   const data = await SPIDataModel.find({ package: "C0402" }, null, {
-    limit: 5000,
+    limit: 1000,
   })
     .select("rawData.height")
     .exec();
